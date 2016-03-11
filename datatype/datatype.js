bd8f66940444be65832c4e831e214bcfe1f90831
liqian <liqian@bilibili.com>
2016-03-11 10:00:49 +0800

    增加datatype

---
+++ datatype/datatype.js
@@ -0,0 +1,14 @@
+/**
+ * 该函数用于完成target的类型判断和校验
+ * 检测target是否为type类型，
+ *     如果是，则直接返回target；
+ *     如果不是，则判断第三个参数是否存在，
+ *         如果存在，则返回默认值
+ *         如果不存在，则强制转换成type类型
+ * @param {String/Number/Boolean/Undefined/Null/Object/Array/Regexp} target
+ * @param {String} type 类型名称
+ * @param {String} def  默认信息（可选）
+ */
+;(function(){
+
+})(window);

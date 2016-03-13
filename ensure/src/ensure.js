9090a9e2740c95afd4f3281adad08a10b2678c25
liqian <liqian@bilibili.com>
2016-03-13 16:41:52 +0800

    updata

--- ensure/src/ensure.js
+++ ensure/src/ensure.js
@@ -5,7 +5,7 @@
  *     如果不是，则判断第三个参数是否存在，
  *         如果存在，则返回默认值
  *         如果不存在，则强制转换成type类型
- * @param {String/Number/Boolean/Undefined/Null/Object/Array/Regexp} target
+ * @param {String/Number/Boolean/Undefined/Null/Object/Array/RegExp} target
  * @param {String} type 类型名称
  * @param {String} def  默认信息（可选）
  */
@@ -15,22 +15,34 @@
   var toStr = Object.prototype.toString;
   var typeArr = ['Boolean','Number','String','Function','Array','Date','RegExp','Object'];
 
+  var dataObj = {
+    'string' : '1',
+    'number' : 1,
+    'undefined' : undefined,
+    'null' : null,
+    'boolean' : true,
+    'object' : {},
+    'array' : [],
+    'function' : function(){},
+    'date' : new Date(),
+    'regexp' : /^\d$/
+  };
+
   for(var i = 0, len = typeArr.length, item; i < len; i++) {
     item = typeArr[i];
     typeObj[ "[object " + item + "]" ] = item.toLowerCase();
   }
 
-  global.d = {
-    type : function(target, type, def){
+  global.myEnsure = {
+    ensure : function(target, type, def){
       var argLens = arguments.length;
       var result;
-      
-      // 如果未输入参数，直接返回 
-      if(argLens < 1) {
+      // 如果未输入参数/ type 不是一个字符串/ type不是数据类型的某一个
+      if(argLens < 1 || argLens > 1 && typeof type !== 'string') {
         return;
       }else {
         if(argLens === 1){                                         // 如果只传入一个参数，返回该参数的类型
-          if(target === null){
+          if(target === null || target === undefined){
             result = String(target);
           }else {
             result = typeObj[ toStr.call(target) ] || "object";
@@ -47,15 +59,33 @@
                  * 如果是转化为string，则调用String（）
                  * 如果是转化为number，则调用Number（）
                  * 如果是转化为boolean，则调用Boolean（）
-                 * 如果是转化为对象
-                 * 如果转化为数组，要分为什么类型，类数组转化数组等
                  */
+                switch(type){
+                  case 'null':
+                    result = null;
+                    break;
+                  case 'undefined':
+                    result = undefined;
+                    break;
+                  default:
+                    /*排除异常情况*/
+                    if(typeObj[ toStr.call(target) ] === 'object' && type === 'function'){
+                      result = function(){}; //myEnsure.ensure({a:1},'function')
+                    }else if(typeof target === 'number' && isNaN(target) && type === 'array'){
+                      result = []; //myEnsure.ensure(NaN,'array')
+                    }else {
+                      result = dataObj[''+ type.toLowerCase() +''].constructor(target);
+                    }
+                }
+                
               }
             }
           }else {                                                  //是null和undefined 就直接返回结果
             result = argLens > 2 ? def : target;
           }
         }
+
+        return result;
       }
     }
   };

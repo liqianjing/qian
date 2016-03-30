d2cb3720bd1f3b86c9b68912427370d3971f9c3f
liqian <liqian@bilibili.com>
2016-03-30 16:49:43 +0800

    slider修改，基本功能完成，示例提供3种形式及其组合

--- slider/slider.js
+++ slider/slider.js
@@ -3,6 +3,8 @@
 
   var SLIDER = 'bili-slider';
   var CLICK = 'click.bilislider';
+  var MOUSEENTER = 'mouseenter.bilislider';
+  var MOUSELEAVE = 'mouseleave.bilislider';
   var ACTIVE = 'bili-active';
   var SIZE = 'sliderSize';
   var Y = 'vertical';
@@ -16,7 +18,7 @@
     animationLoop: true,            // Boolean: 是否循环播放
 
     startAt: 0,                     // Integer: 开始播放的 slide，从 0 开始计数
-    slideAuto: false,                // Boolean: 是否自动播放
+    slideAuto: false,               // Boolean: 是否自动播放
     slideshowSpeed: 5000,           // Integer: ms 滚动间隔时间
     animationSpeed: 600,            // Integer: ms 动画滚动速度
     initDelay: 0,                   // Integer: ms 首次执行动画的延迟
@@ -26,24 +28,14 @@
 
     controlThumbnail: false,        // Boolean: 是否创建缩略图
     controlNav: true,               // Boolean: 是否创建控制点
-    directionNav: false,             // Boolean: 是否创建上/下一个按钮（previous/next）
+    directionNav: false,            // Boolean: 是否创建上/下一个按钮（previous/next）
     prevText: "Previous",           // String: 上一个按钮文字
-    nextText: "Next",                // String: 下一个按钮文字
-    
-    // itemWidth: 0,                   // Integer: slide 宽度，多个同时滚动时设置
-    // itemMargin: 0,                  // Integer: slide 间距
-    // minItems: 1,                    // Integer: 最少显示 slide 数, 与 `itemWidth` 相关
-    // maxItems: 0,                    // Integer: 最多显示 slide 数, 与 `itemWidth` 相关
-    // move: 0,                        // Integer: 一次滚动移动的 slide 数量，0 - 滚动可见的 slide
-
-    // start: function(){},            // Callback: function(slider) - 初始化完成的回调
-    // before: function(){},           // Callback: function(slider) - 每次滚动开始前的回调
-    // after: function(){},            // Callback: function(slider) - 每次滚动完成后的回调
+    nextText: "Next",               // String: 下一个按钮文字
     end: function(){},              // Callback: function(slider) - 执行到最后一个 slide 的回调
-    // added: function(){},            // Callback: function(slider) - slide 添加时触发
-    // removed: function(){}           // Callback: function(slider) - slide 被移除时触发
+    start: function(){}             // Callback: function(slider) - 执行到最前面一个 slide 的回调
   };
 
+  // 入口函数
   function compInit($el){
     var el = $el[0];
     var opts = $.data(el, SLIDER); //配置参数
@@ -92,7 +84,9 @@
     // 获取外框的大小，由此定义里面盒子的大小
     var wid = $el.width();
     var hei = $el.height();
-
+    $el.css({
+      'position' : 'relative'
+    });
     $view.css({
       width: wid + 'px',
       height: hei + 'px'
@@ -151,7 +145,7 @@
     if(opts.controlNav) {
       str += '<ol class="bili-control-nav bili-control-paging">';
       for(var i = 0; i < lens; i++){
-        str += '<li class="'+ ( startAt === i ? "bili-active" : "") +'"><a class="bili-control-item">'+ (i+1) +'</a><i></i></li>';
+        str += '<li class="bili-control-item '+ ( startAt === i ? "bili-active" : "") +'" data-index="'+ i +'"><a href="javascript:;">'+ (i+1) +'</a><i></i></li>';
       }
       str += '</ol>';
     }
@@ -160,7 +154,7 @@
     if(opts.controlThumbnail) {
       str += '<ol class = "bili-control-nav bili-control-thumbs">';
       for(var i = 0; i < lens; i++){
-        str += '<li class="'+ ( startAt === i ? "bili-active" : "") +'"><img src="'+ opts.imgUrl[i] +'" class="bili-active"><i></i></li>';
+        str += '<li class="bili-control-item '+ ( startAt === i ? "bili-active" : "") +'" data-index="'+ i +'"><img src="'+ opts.imgUrl[i] +'" class="bili-active"><i></i></li>';
       }
       str += '</ol>';
     }
@@ -171,7 +165,7 @@
       str += '<p class="bili-btn bili-btn-right">' + opts.nextText + '</p>';
     }
 
-    $el.append(str);
+    $('div.bili-slider',$el).append(str);
 
     // 4. 进行事件的绑定
     bindEvent($el);
@@ -186,29 +180,30 @@
     var $slide = $('ul.bili-slides',$el);
     var $leftBtn = $('p.bili-btn-left',$el);
     var $rightBtn = $('p.bili-btn-right',$el);
+    var $imgs = $('li.bili-slides-item',$slide);
 
-    //绑定下面的小点(或者缩略图)
+    // 绑定下面的小点(或者缩略图)
     $lis.off(CLICK).on(CLICK,function(){
       var $this = null;
       var curIndex = 0;
-      var $imgs = $('li.bili-slides-item',$slide);
 
       $this = $(this);
       //curIndex = $this.prevAll().length; // 当前点击的li的索引值
-      curIndex = $this.text() - 1;
+      curIndex = parseInt($this.attr('data-index'));
       $.data(el, CURRENT, curIndex);
 
       //点击的位置变化
       $lis.removeClass(ACTIVE);
       $this.addClass(ACTIVE);
 
+      // todo
       //对应的上面的图片变化（增加动画）
       if(opts.animation === 'fade') {
         $imgs.fadeOut(opts.animationSpeed, 'linear', function(){
           $imgs.removeClass(ACTIVE);
         });
         $imgs.eq(curIndex).fadeIn(opts.animationSpeed, 'linear', function(){
-          $(this).addClass(ACTIVE);
+          $this.addClass(ACTIVE);
         });
       }else {
         if(supportAnimate) {
@@ -217,7 +212,6 @@
           jqAnimate($el,$slide,curIndex);
         }
       }
-
     });
 
     // 处理自动切换
@@ -225,30 +219,73 @@
 
     // 左按钮绑定事件
     $leftBtn.off(CLICK).on(CLICK,function(){
-
-      // 做循环处理
-      if(opts.animationLoop){
-        
-      }
+      clickBtn($el,'left');
     });
 
     // 右按钮绑定事件
     $rightBtn.off(CLICK).on(CLICK,function(){
-
-      // 做循环处理
-      if(opts.animationLoop){
-        
-      }
+      clickBtn($el,'right');
     });
   }
 
+  // 点击左右按钮的处理部分
+  function clickBtn($el, direction){
+    var el = $el[0];
+    var opts = $.data(el, SLIDER); //配置参数
+    var lens = $.data(el, SIZE); //总长度
+    var curIndex; //当前显示的
+    var navLen = $('ol.bili-control-nav',$el).size();
+    var $lis = $('ol.bili-control-nav li',$el);
+    var $slide = $('ul.bili-slides',$el);
+
+    curIndex = $.data(el, CURRENT);
+
+    if( direction === 'left') {
+      --curIndex;
+      if(curIndex < 0) {
+        if(opts.animationLoop) {
+          curIndex = lens-1;
+        }else {
+          opts.end();
+          return;
+        }
+      }
+    }
+
+    if( direction === 'right') {
+      ++curIndex;
+      if(curIndex >= lens) {
+        if(opts.animationLoop) {
+          curIndex = 0;
+        }else {
+          opts.end();
+          return;
+        }
+      }
+    }
+
+    //移动(默认先去找是否初始化了控制点，如果有就直接trigger，否则的话再重新计算滚动)
+    if(navLen > 0) {
+      $lis.eq(curIndex).trigger(CLICK);
+    }else { // 左右移动
+      if(supportAnimate) {
+        animate($el,$slide,curIndex);
+      }else {
+        jqAnimate($el,$slide,curIndex);
+      }
+    }
+    //记录当前
+    $.data(el, CURRENT, curIndex);
+
+  }
+
+  // 自动切换
   function animateAuto($el){
     var el = $el[0];
     var opts = $.data(el, SLIDER); //配置参数
     var $lis = $('ol.bili-control-nav li',$el);
     var lens = $.data(el, SIZE);
     var current = 0;
-
     if(opts.slideAuto) { //如果需要自动播放
       timer = setInterval(function(){
         current = $.data(el, CURRENT);
@@ -257,40 +294,25 @@
     }
   }
   
-  // 滚动的函数
+  // 滚动的函数(如果自动播放，则默认循环)
   function showAuto($el, n, count) {
     var el = $el[0];
     var $lis = $('ol.bili-control-nav li',$el);
     var opts = $.data(el, SLIDER);
 
-    // 是否循环播放
-    // if(n >= (count -1)){
-    //   if( opts.animationLoop ){
-    //     n = 0;
-    //   }else {
-    //     opts.end(n);
-    //   }
-    // }else {
-    //   n = ++n;
-    // }
-
     // updata: 如果自动播放，那么默认循环
-    n > (count -1) ? 0 : ++n;
-
+    n = n >= (count -1) ? 0 : ++n;
     $lis.eq(n).trigger(CLICK);
     $.data(el, CURRENT, n);
   }
 
-  function animateLoop($el){
-
-  }
-
   // 支持css3动画的动画
   // 根据animationSpeed，执行动画
+  // 和下面的jqAnimate的函数只会执行一个
   function animate($el,tar,curIndex){
     var el = $el[0];
     var opts = $.data(el, SLIDER); //配置参数
-    var speed = opts.animationSpeed; //执行动画的时间
+    //var speed = opts.animationSpeed; //执行动画的时间
     var wid = $el.width();
     var hei = $el.height();
     var animateVal = "translateX(-" + wid * curIndex + "px)";
@@ -305,6 +327,7 @@
   }
 
   // 不支持css3的，用jq的动画
+  // 和上面的animate的函数只会执行一个
   function jqAnimate($el,tar,curIndex){
     var el = $el[0];
     var opts = $.data(el, SLIDER); //配置参数
@@ -320,11 +343,6 @@
         top : -(hei * curIndex) + 'px'
       };
     }
-
-    // animate
-    tar.animate(scroll, opts.animationSpeed, function(){
-
-    });
   }
 
   // 判断当前的浏览器是否支持translate和transform
@@ -345,7 +363,7 @@
     var $el = this;
     var el = $el[0];
 
-    if(typeof param === 'string') { //如果传入的是字符串
+    if(typeof param === 'string') { //如果传入的是字符串,则调用方法
 
     }else { //如果是对象，则为初始化
       var options = $.extend({}, paramDefault, param); //初始化的配置参数
